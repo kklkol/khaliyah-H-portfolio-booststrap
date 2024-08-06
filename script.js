fb745b94e14f5379dd11ab4e3364f7a272e11123
kklkol <harleykhaliyah@gmail.com>
2024-08-06 16:05:14 +0000

    built an age verification app for my home page

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-console.log("khaliyah")
- 
+// console.log("khaliyah")
+
+// var prompt=prompt('what is your name?')
+// console.log(prompt)
+
+//var prompt=prompt('what is your age?')
+//console.log(prompt)
+
+
+
+function verifyAge() {
+  var age = prompt('What is your age?');
+  console.log(age)
+
+  if (age < 18) {
+    alert('You are a minor!');
+  } else if (age >=18 && age<=60){
+    alert('You are an adult!')
+  } else{
+    alert('You are a senior citizen! ')
+  }
+}
